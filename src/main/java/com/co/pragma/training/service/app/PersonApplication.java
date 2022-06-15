43e28d5f4455c19b6e225141a7d65ce4895d95cc
Relari <renzolavador@gmail.com>
2022-06-15 11:24:53 -0500

    feat: initial project

---
+++ src/main/java/com/co/pragma/training/service/app/PersonApplication.java
@@ -0,0 +1,21 @@
+package com.co.pragma.training.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b>Class:</b> PersonApplication.
+ * <b>Company:</b> PRAGMA SOLUCIONES TECNOLÓGICAS PERU S.A.C.
+ *
+ * @author RLR
+ * @version 1.0.0
+ */
+
+@SpringBootApplication
+public class PersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApplication.class);
+	}
+
+}
